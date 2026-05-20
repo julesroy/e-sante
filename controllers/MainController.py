8faa9770a944b52d9b53850cd0041e16ed8da522
Nomisyorel <simon.leroy@student.junia.com>
2026-05-20 17:43:52 +0200

    feat : upload + visu de l'image
    ajout de l'upload des images + visuel de l'image

--- controllers/MainController.py
+++ controllers/MainController.py
@@ -1,21 +1,28 @@
+from PyQt6.QtWidgets import QFileDialog
+
+
 class MainController:
     def __init__(self, model, view):
         self.model = model
         self.view = view
 
-        # connecte les signaux de la vue aux méthodes du contrôleur
-        # ça servira à faire communiquer les actions de l'utilisateur (clics sur les boutons) avec les modifications du modèle et la mise à jour de la vue
+        # Connecte les signaux de l'interface aux fonctions du contrôleur
         self._connect_signals()
 
     def _connect_signals(self):
-        self.view.btn_increment.clicked.connect(self.handle_increment)  # si on reçoit le signal "clicked" du bouton d'incrémentation, on appelle la méthode handle_increment
-        self.view.btn_reset.clicked.connect(self.handle_reset)  # si on reçoit le signal "clicked" du bouton de réinitialisation, on appelle la méthode handle_reset
+        # bouton d'upload
+        self.view.btn_upload.clicked.connect(self.handle_upload)
 
-    def handle_increment(self):
-        self.model.increment_counter()  # on demande D'ABORD au modèle d'incrémenter le compteur
-        self.view.update_counter_display(self.model.counter)  # puis on met à jour l'affichage de la vue avec la nouvelle valeur du compteur (qu'on récupère du modèle)
+    def handle_upload(self):
+        """Gère la sélection du fichier et demande à la vue de l'afficher."""
+        # explorateur de fichiers
+        file_path, _ = QFileDialog.getOpenFileName(
+            self.view,
+            "Sélectionner une radiographie",
+            "",
+            "Images (*.png *.jpg *.jpeg)",
+        )
 
-    def handle_reset(self):
-        # même principe ici que pour l'incrémentation
-        self.model.reset_counter()
-        self.view.update_counter_display(self.model.counter)
+        # Si image on l'affiche
+        if file_path:
+            self.view.display_medical_image(file_path)
--- views/MainView.py
+++ views/MainView.py
@@ -1,12 +1,16 @@
 from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QPushButton, QLabel
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap , QGuiApplication
 
 
 class MainView(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("MVC PyQt6")
-        self.resize(300, 200)
+        self.setWindowTitle("Notre super projet e-santé")
+        self.resize(500, 300)
+        
+        # 2. Centrage automatique de la fenêtre au milieu de l'écran
+        self.center_on_screen()
 
         # composants
         self.central_widget = QWidget()
@@ -14,17 +18,39 @@ class MainView(QMainWindow):
         self.layout = QVBoxLayout(self.central_widget)
 
         # widgets
-        self.label_counter = QLabel("Compteur : 0")
-        self.label_counter.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.btn_upload = QPushButton("Ouvrir une radiographie")
+        self.layout.addWidget(self.btn_upload)
 
-        self.btn_increment = QPushButton("+1")
-        self.btn_reset = QPushButton("Reset")
+        # visualisation img
+        self.image_display = QLabel("Aucune radiographie chargée.")
+        self.image_display.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        # style
+        self.image_display.setStyleSheet(
+            "background-color: #111111; "
+            "color: #888888; "
+            "border: 2px dashed #333333; "
+            "min-height: 500px; "
+            "font-size: 16px;"
+        )
+        self.layout.addWidget(self.image_display)
 
-        # assemblage
-        self.layout.addWidget(self.label_counter)
-        self.layout.addWidget(self.btn_increment)
-        self.layout.addWidget(self.btn_reset)
-
-    def update_counter_display(self, value: int):
-        """Met à jour l'affichage avec la nouvelle valeur."""
-        self.label_counter.setText(f"Compteur : {value}")
+    def display_medical_image(self, file_path: str):
+        """Affiche la radio dans le viewer et ajuste sa taille proprement."""
+        pixmap = QPixmap(file_path)
+        # Ajuste l'image aux dimensions du viewer (on garde les ratios) 
+        scaled_pixmap = pixmap.scaled(
+            self.image_display.width(),
+            self.image_display.height(),
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.SmoothTransformation,
+        )
+        self.image_display.setPixmap(scaled_pixmap)
+    def center_on_screen(self):
+        """Calcule le centre de l'écran de l'utilisateur et y place la fenêtre."""
+        screen = QGuiApplication.primaryScreen()
+        if screen:
+            screen_geometry = screen.geometry()
+            x = (screen_geometry.width() - self.width()) // 2
+            y = (screen_geometry.height() - self.height()) // 2
+            self.move(x, y)
