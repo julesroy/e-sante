b6ef8ce3398963542eb92a0e593119d660980d74
Camelibob <c33mdurand@gmail.com>
2026-06-03 12:52:24 +0200

    feat: ajout des declarations pour export

---
+++ database/crud/__init__.py
@@ -0,0 +1,11 @@
+from .patients import (
+    creer_patient,
+    rechercher_patient,
+    get_tous_les_patients,
+    supprimer_patient,
+)
+from .images import (
+    sauvegarder_image,
+    get_images_patient,
+    supprimer_image,
+)
